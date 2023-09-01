91a2fae19afdb4d35509c869d412e6c1568b0d9c
matias1364 <matiasleoncontreras@gmail.com>
2023-09-01 20:04:25 -0300

    foreach

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-puppe = require('puppeteer')              
-        
-inicio() 
+const puppeteer = require('puppeteer');
+
+inicio();
 
 async function inicio() {
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const pagina = await browser.newPage();
 
-  browser = await puppe.launch({ headless: false });
+  await pagina.goto("https://www.sporting.com.ar/sporting/calzado/zapatillas");
 
-  pagina = await browser.newPage();
-
-  await pagina.goto("https://www.dolarsi.com.ar/func/tool4.php");
-
-  let text = await pagina.evaluate(() => {
-    return document.querySelector("#v1").innerText;
-  });
+  const num = Array.from({ length: 20 }, (_, index) => index + 1);
+  const text = await pagina.evaluate((num) => {
+    const result = [];
+    num.forEach((num) => {
+      const selector = `#gallery-layout-container > div:nth-child(${num})`;
+      const element = document.querySelector(selector);
+      if (element) {
+        result.push(element.innerText);
+      }
+    });
+    return result;
+  }, num);
 
   console.log(text);
-  
+
   setTimeout(() => {
     browser.close();
-  }, 1000);
-}                                        
+  }, 3000);
+}                               
